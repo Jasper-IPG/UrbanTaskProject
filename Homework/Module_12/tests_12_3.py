8683d7684beeb575c4459ea59ff958b559ab8171
Jasper.IPG <jasper.ipg@gmail.com>
2024-10-25 13:40:01 +0600

    "Заморозка кейсов"

--- Homework/Module_12/tests_12_3.py
+++ Homework/Module_12/tests_12_3.py
@@ -1,7 +1,46 @@
 # "Заморозка кейсов"
 import unittest
-import runner
-import runner_and_tournament as r_a_t
+
+
+class Runner:
+    def __init__(self, name, speed=5):
+        self.name = name
+        self.distance = 0
+        self.speed = speed
+
+    def run(self):
+        self.distance += self.speed * 2
+
+    def walk(self):
+        self.distance += self.speed
+
+    def __str__(self):
+        return self.name
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.name == other
+        elif isinstance(other, Runner):
+            return self.name == other.name
+
+
+class Tournament:
+    def __init__(self, distance, *participants):
+        self.full_distance = distance
+        self.participants = list(participants)
+
+    def start(self):
+        finishers = {}
+        place = 1
+        while self.participants:
+            for participant in self.participants:
+                participant.run()
+                if participant.distance >= self.full_distance:
+                    finishers[place] = participant
+                    place += 1
+                    self.participants.remove(participant)
+
+        return finishers
 
 
 class RunnerTest(unittest.TestCase):
@@ -9,22 +48,22 @@ class RunnerTest(unittest.TestCase):
 
     @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_walk(self):
-        first = runner.Runner('Alex')
+        first = Runner('Alex')
         for _ in range(10):
             first.walk()
         self.assertEqual(first.distance, 50)
 
     @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_run(self):
-        second = runner.Runner('Pavel')
+        second = Runner('Pavel')
         for _ in range(10):
             second.run()
         self.assertEqual(second.distance, 100)
 
     @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_challenge(self):
-        third = runner.Runner('Tim')
-        fourth = runner.Runner('Mark')
+        third = Runner('Tim')
+        fourth = Runner('Mark')
         for _ in range(10):
             third.walk()
             fourth.run()
@@ -39,25 +78,25 @@ class TournamentTest(unittest.TestCase):
         cls.all_results = {}
 
     def setUp(self):
-        self.first = r_a_t.Runner('Усэйн', 10)
-        self.second = r_a_t.Runner('Андрей', 9)
-        self.third = r_a_t.Runner('Ник', 3)
+        self.first = Runner('Усэйн', 10)
+        self.second = Runner('Андрей', 9)
+        self.third = Runner('Ник', 3)
 
     @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_tournament_1(self):
-        t = r_a_t.Tournament(90, self.first, self.third)
+        t = Tournament(90, self.first, self.third)
         TournamentTest.all_results.update({1: t.start()})
         self.assertTrue(TournamentTest.all_results[1][max(TournamentTest.all_results[1])] == 'Ник')
 
     @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_tournament_2(self):
-        t = r_a_t.Tournament(90, self.second, self.third)
+        t = Tournament(90, self.second, self.third)
         TournamentTest.all_results.update({2: t.start()})
         self.assertTrue(TournamentTest.all_results[2][max(TournamentTest.all_results[2])] == 'Ник')
 
     @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_tournament_3(self):
-        t = r_a_t.Tournament(90, self.first, self.second, self.third)
+        t = Tournament(90, self.first, self.second, self.third)
         TournamentTest.all_results.update({3: t.start()})
         self.assertTrue(TournamentTest.all_results[3][max(TournamentTest.all_results[3])] == 'Ник')
 
