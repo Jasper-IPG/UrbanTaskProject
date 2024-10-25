a76e088db511357cd63113a26c1bf8dfc8e8c50f
Jasper.IPG <jasper.ipg@gmail.com>
2024-10-25 13:17:54 +0600

    "Проверка на выносливость"

--- Homework/Module_12/module_12_1.py
+++ Homework/Module_12/module_12_1.py
@@ -6,21 +6,18 @@ import runner
 class RunnerTest(unittest.TestCase):
     is_frozen = False
 
-    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_walk(self):
         first = runner.Runner('Alex')
         for _ in range(10):
             first.walk()
         self.assertEqual(first.distance, 50)
 
-    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_run(self):
         second = runner.Runner('Pavel')
         for _ in range(10):
             second.run()
         self.assertEqual(second.distance, 100)
 
-    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_challenge(self):
         third = runner.Runner('Tim')
         fourth = runner.Runner('Mark')
