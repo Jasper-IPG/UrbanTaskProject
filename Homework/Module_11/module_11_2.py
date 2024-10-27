3674073dafc3de6183b59480b6ad4d8a6e62767f
Jasper.IPG <jasper.ipg@gmail.com>
2024-10-27 06:20:13 +0600

    "Интроспекция"

--- Homework/Module_11/module_11_2.py
+++ Homework/Module_11/module_11_2.py
@@ -2,25 +2,15 @@
 import inspect
 
 
-class Object:
-    def __init__(self, obj):
-        self.obj = obj
-
-    def introspection_info(self):
-        result = {}
-        if type(self.obj) is int:
-            result.update({'type': 'int'})
-        else:
-            result.update({'type': 'not int'})
-        if hasattr(self, 'obj'):
-            result.update({'attributes': [...]})
-        if dir(self.obj):
-            result.update({'methods': ['__abs__', '__add__', ...]})
-        if inspect.getmodule(self):
-            result.update({'module': '__main__'})
-        return result
+def introspection_info(obj):
+    result = {'type': type(obj),
+              'attributes': dir(obj),
+              'methods': [method for method in dir(type(obj)) if callable(getattr(obj, method))],
+              'module': inspect.getmodule(introspection_info)}
+    return result
 
 
-number_info = Object(42)
-print(Object.introspection_info(number_info))
-# print(number_info)
+number_info = introspection_info(42)
+print(number_info)
+
+
