2a5fbde6c1bc1ffb17cc5fab5ec7fd9249f1fa63
Jasper.IPG <jasper.ipg@gmail.com>
2024-10-28 12:43:33 +0600

    "Интроспекция"

--- Homework/Module_11/module_11_2.py
+++ Homework/Module_11/module_11_2.py
@@ -4,13 +4,11 @@ import inspect
 
 def introspection_info(obj):
     result = {'type': type(obj),
-              'attributes': dir(obj),
+              'attributes': [attr for attr in dir(obj) if not callable(getattr(obj, attr))],
               'methods': [method for method in dir(type(obj)) if callable(getattr(obj, method))],
-              'module': inspect.getmodule(introspection_info)}
+              'module': obj.__class__.__module__}
     return result
 
 
 number_info = introspection_info(42)
 print(number_info)
-
-
